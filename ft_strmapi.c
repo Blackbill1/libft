226bc51eed0ad57ca30efaed9e6476ed30a08c0e
tle-dref <thomas.ledref@gmail.com>
2024-10-15 03:39:08 +0200

    fini la partie obligatoire

---
+++ ft_strmapi.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	unsigned int	i;
+
+	res = malloc(ft_strlen((char *)s) * sizeof(char));
+	if(!res)
+		return (NULL);
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
